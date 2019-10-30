964091d11525b85601804854e8131551b0807542
Mark Moran <markmoran@gmail.com>
2019-10-29 18:22:11 -1000

    added escape text to splash screen

--- text.lua
+++ text.lua
@@ -3,7 +3,7 @@ use this file to set fonts, print out text to the screen, etc.
 ]]
 
 -- set fonts
-defaultFont = love.graphics.newFont(20)
+defaultFont = love.graphics.newFont(14)
 
 -- splash Screen Text function
 function splashText()
@@ -14,5 +14,6 @@ function splashText()
   love.graphics.setFont(defaultFont)
   love.graphics.print("Press Return to start the game.", arenaWidth / 2 - 100, arenaHeight / 2)
   love.graphics.print("Arrow keys move the player around", arenaWidth / 2 - 100, arenaHeight / 2 + 20)
+  love.graphics.print("Press escape to leave the game", arenaWidth / 2 - 100, arenaHeight / 2 + 40)
 
 end -- splashText
