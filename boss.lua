83264667c7528f9a85910f88ff638a9b89353d0b
twitchums <twitchums@users.noreply.github.com>
2019-11-05 13:29:20 -1000

    Added enemy/boss descriptions as a guide in the boss.lua file.

--- boss.lua
+++ boss.lua
@@ -1,2 +1,43 @@
 -- this is where we can put functions and information related to the boss
 -- or enemies
+
+
+-- ENEMIES
+
+--1) Fae
+
+  --a) Wisps
+  -- Have already around the map and will chase player when they get close.
+
+
+--2) Dinosaurs
+
+  --a) Raptor
+  -- Fast enemy, spread around the map, chases player when they get close.
+
+  --b) Stegosaurus
+  -- Slow enemy, spread around the map, peaceful until hit.
+
+  --c) Spinosaurus
+  -- Slow enemy, hunts down the player around the map.
+
+
+--3) Aliens
+
+  --a) Soldier
+  -- Spread around the map, shoots the player on sight, doesn't move towards the player.
+
+  --b) Fodder
+  -- Spread around the map, chases down the player.
+
+
+  -- BOSSES
+
+  --1) The "Demon" Knight
+  -- Very slow enemy, has slash attacks like the player does but reaches a farther area. Shoots at the player occassionally but not a lot.
+
+  --2) T-rex
+  -- Very big and slow enemy, charges in a straight line at a the player's position.
+
+  --3) UFO/Big Alien
+  -- Big and slow enemy. Shoots at the player, charges at the player, and has an area of effect attack.
