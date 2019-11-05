c8f1952f599b92fc77a5538753902810b7d26d8e
Mark Moran <markmoran@gmail.com>
2019-11-04 19:26:27 -1000

    Added some more instructions to the splash screen

--- text.lua
+++ text.lua
@@ -12,8 +12,10 @@ function splashText()
   love.graphics.rectangle('fill', 0, 0, 800, 600)
   love.graphics.setColor(0, 0, 0)
   love.graphics.setFont(defaultFont)
-  love.graphics.print("Press Return to start the game.", arenaWidth / 2 - 100, arenaHeight / 2)
-  love.graphics.print("Arrow keys move the player around", arenaWidth / 2 - 100, arenaHeight / 2 + 20)
-  love.graphics.print("Press escape to leave the game", arenaWidth / 2 - 100, arenaHeight / 2 + 40)
+  love.graphics.print("1. Press Return to start the game.", arenaWidth / 2 - 100, arenaHeight / 2)
+  love.graphics.print("2. Press escape to leave the game", arenaWidth / 2 - 100, arenaHeight / 2 + 20)
+  love.graphics.print("GAME CONTROLS:", arenaWidth / 2 - 100, arenaHeight / 2 + 80)
+  love.graphics.print("Arrow keys move the player around", arenaWidth / 2 - 100, arenaHeight / 2 + 100)
+  love.graphics.print("Space button shoots", arenaWidth / 2 - 100, arenaHeight / 2 + 120)
 
 end -- splashText
