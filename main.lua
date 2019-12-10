28d749d0dc068a47b7090fb06ffe2e4ea6955ec1
Mark Moran <mark@mark-macbook-pro.local>
2019-12-10 13:19:08 -1000

    fixing level 2 barriers

--- main.lua
+++ main.lua
@@ -245,7 +245,7 @@ end
     for j = 1, #levelMap2 do
       local row = levelMap2[j]
       for k = 1, #row do
-        if levelMap2[j][k] >= 20 or levelMap2[j][k] <= 32 then -- if the tile is a 1 then we add it to the bump world in this example.
+        if levelMap2[j][k] == 1 or levelMap2[j][k] == 2 or (levelMap2[j][k] >= 20 and levelMap2[j][k] <= 32) then -- if the tile is a 1 then we add it to the bump world in this example.
           --love tables start at 1 but the window dimensions start at 0, so we minus 1 to start at 0. it's weird but that's how it goes.
           addBlock((k - 1) * TILE_SIZE, (j - 1) * TILE_SIZE, TILE_SIZE, TILE_SIZE)
         end
